c3cf17e37d8e3ed49c97789225c79fed44ef7a60
ppumstr307 <sainichetanstudy@gmail.com>
2025-02-05 22:13:36 +0530

    second commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+posts = [
+    {
+        'author':'jamal',
+        'title':'Post 1',
+        'content':'This is the first post',
+        'date':'18 jan 1993'
+    },
+    {
+        'author':'jinco',
+        'title':'Post 2',
+        'content':'This is the second post',
+        'date':'19 jan 1993'
+    }
+]
+def home(request):
+    context={
+        'posts':posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html',{'title':'About'})
+
+
+
