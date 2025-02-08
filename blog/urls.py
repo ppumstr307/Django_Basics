3cf8ec64264db42bf75ddf0dba376f4ea89f0773
ppumstr307 <sainichetanstudy@gmail.com>
2025-02-08 18:36:29 +0530

    third commit

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='Blog-Home'),
-    path('about/', views.about, name='Blog-about'),
+    path('', views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Posts
 
 
-posts = [
-    {
-        'author':'jamal',
-        'title':'Post 1',
-        'content':'This is the first post',
-        'date':'18 jan 1993'
-    },
-    {
-        'author':'jinco',
-        'title':'Post 2',
-        'content':'This is the second post',
-        'date':'19 jan 1993'
-    }
-]
 def home(request):
     context={
-        'posts':posts
+        'posts':Posts.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
